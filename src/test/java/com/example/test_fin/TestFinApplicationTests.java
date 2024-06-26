2103161c430e4fbcd5240b46e70bc8be84995106
young-jin-seo <young-jin.seo@navercorp.com>
2024-06-26 23:12:37 +0900

    init test

---
+++ src/test/java/com/example/test_fin/TestFinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_fin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
